03ee6f4d41f3f706b5e9eaebafe16ff4265c48e6
jaren11 <jaren11@gmail.com>
2022-05-19 19:24:48 -0600

    added start script

--- app.js
+++ app.js
@@ -161,6 +161,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render(`error`, { err })
 })
 
-app.listen(8080, () => {
-    console.log("LISTENING ON PORT 8080");
+
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+    console.log(`LISTENING ON PORT ${port}`);
 })
